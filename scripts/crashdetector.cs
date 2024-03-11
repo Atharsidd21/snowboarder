4830285c33337dcfc0f2007906d0d29f751d620b
Atharsidd21 <95632597+Atharsidd21@users.noreply.github.com>
2024-03-11 21:26:57 +0530

    Snowboarder

---
+++ scripts/crashdetector.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class crashdetector : MonoBehaviour
+{
+     [SerializeField] float loadDelay = 0.5f;
+      [SerializeField] ParticleSystem crashEffect;
+        [SerializeField] AudioClip crashSFX;
+        bool hasCrashed = false;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Ground" && !hasCrashed)
+        {
+            hasCrashed = true;
+            FindObjectOfType<playercontroller>().DisableControls();
+            crashEffect.Play();
+                GetComponent<AudioSource>().PlayOneShot(crashSFX);
+           Invoke("ReloadScene" , loadDelay);
+        }
+    }
+    void ReloadScene()
+   {
+       SceneManager.LoadScene(0);
+   }
+}
